0f5327be8ad44f7b80271108cf6992e8ab60be83
Desktop\Jonathan Martínez <jonamtz7774@gmail.com>
2026-06-26 10:25:37 -0600

    Section Consultoria added to home view.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Hero from "@/components/home/hero/hero";
+import Consultoria from "@/components/home/hero/consultoria";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      {/* Espaciador temporal para probar el parallax */}
+      <Consultoria />
       <div className="h-screen" />
     </>
   );
