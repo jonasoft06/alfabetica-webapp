032e710bf34c54e91342f56818b78980ae190c73
Desktop\Jonathan Martínez <jonamtz7774@gmail.com>
2026-07-09 17:22:25 -0600

    fix: correct consulting layout on mobile and large screens

--- src/components/home/hero/hero.tsx
+++ src/components/home/hero/hero.tsx
@@ -27,7 +27,7 @@ export default function Hero() {
         className="pointer-events-none absolute top-1/2 hidden max-w-none -translate-y-[40%] select-none lg:-right-28 lg:block lg:w-[340px] xl:-right-44 xl:w-[460px] 2xl:-right-55 2xl:w-[600px]"
       />
 
-      <div className="relative z-10 mx-auto flex max-w-3xl flex-col items-center px-6 pt-32 pb-20 text-center lg:pt-56 lg:pb-32 xl:pt-64">
+      <div className="relative mx-auto flex max-w-3xl flex-col items-center px-6 pt-32 pb-20 text-center lg:pt-56 lg:pb-32 xl:pt-64">
         <h1 className="font-heading text-4xl leading-tight text-alf-eerie-black md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl">
           Armamos <b className="font-bold">tu libro</b>
           <br />
